56aac5804772a09c74422ebebd38ee7a368e009b
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-20 20:17:03 +0530

    DEV: Modified sample root api response

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const getSampleJSON = (_req: Request, res: Response) => {
-    res.status(200).json({ message: 'successfully deployed app to aws lambda' });
+    res.status(200).json({ message: 'successfully deployed app to aws lambda!' });
 };
 
 export default getSampleJSON;
