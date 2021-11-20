a67af96aa4c6caecbce00358ad9f803786e9be87
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-20 15:08:12 +0530

    DEPLOY: Serverless lambda deploy

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import express from 'express';
-import ENV from './constants/env';
+import serverless from 'serverless-http';
+// import ENV from './constants/env';
 import loggerHandler from './middlewares/loggerHandler';
 import ROUTER from './routes';
-import LOGGER from './utils/logger';
+// import LOGGER from './utils/logger';
 
-const handler = () => {
-    const app = express();
+const app = express();
 
-    // Middlesware
-    app.use(loggerHandler);
+// Middlesware
+app.use(loggerHandler);
 
-    // Routes
-    app.use('/api/v1/', ROUTER);
+// Routes
+app.use('/api/v1/', ROUTER);
 
-    app.listen(ENV.PORT, () => LOGGER.info(`Server listening on port ${ENV.PORT}`));
-};
+// app.listen(ENV.PORT, () => LOGGER.info(`Server listening on port ${ENV.PORT}`));
 
-handler();
+module.exports.handler = serverless(app);
