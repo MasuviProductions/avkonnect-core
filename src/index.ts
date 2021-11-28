26071047e389420dce41bacebf8b47b221b504a3
Akash Krishnan <akashkrishnan98@gmail.com>
2021-11-28 10:22:29 +0530

    DEV: Added cors middleware to express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import authHandler from './middlewares/authHandler';
 import errorHandler from './middlewares/errorHandler';
 import loggerHandler from './middlewares/loggerHandler';
@@ -8,14 +9,17 @@ import USER_ROUTER from './routes/user';
 
 const APP = express();
 
+APP.use(cors());
 APP.use(express.json());
 
 // Middlesware
 APP.use(loggerHandler);
+
+APP.use('/api/v1/test', ROUTER);
+
 APP.use(authHandler);
 
 // Routes
-APP.use('/api/v1/', ROUTER);
 APP.use('/api/v1/auth', AUTH_ROUTER);
 APP.use('/api/v1/users', USER_ROUTER);
 
