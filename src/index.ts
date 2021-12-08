562ab551b59bdef1e7df4f485fed8585571a630c
Akash Krishnan <akashkrishnan98@gmail.com>
2021-12-08 16:30:17 +0530

    DEV: Cors allowed

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const APP = express();
 initDynamoDB();
 
 // Middleware
-APP.use(cors({ credentials: true, origin: true }));
+APP.use(cors({ origin: true }));
 APP.use(helmet());
 APP.use(express.json());
 APP.use(loggerHandler);
