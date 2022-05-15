6bf7e0acc716f466015f08808727c83775b9809e
Akash Krishnan <akashkrishnan98@gmail.com>
2022-05-15 14:32:14 +0530

    DEV: Added region to SQS init

--- src/utils/queue/index.ts
+++ src/utils/queue/index.ts
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk';
+import ENV from '../../constants/env';
 
-const SQS_QUEUE = new AWS.SQS();
+const SQS_QUEUE = new AWS.SQS({ region: ENV.AWS.REGION });
 
 export default SQS_QUEUE;
