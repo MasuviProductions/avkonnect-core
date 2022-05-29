9fcc45e10d7ee8cd13935e4248d0226194332a31
Akash Krishnan <akashkrishnan98@gmail.com>
2022-05-29 18:13:44 +0530

    feat: service token integration

--- src/declaration/index.d.ts
+++ src/declaration/index.d.ts
@@ -3,6 +3,7 @@ import { IAuthUser } from '../interfaces/api';
 declare module 'express' {
     interface Request {
         authUser?: Readonly<IAuthUser>;
+        isUserToken?: boolean;
     }
 }
 export = 'express';
